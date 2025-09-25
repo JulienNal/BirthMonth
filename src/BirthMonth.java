6b6c2f6a2ebe66fce9942ac258aa636b93ed5976
JulienNal <naljs@mail.uc.edu>
2025-09-25 15:08:25 -0400

    Initial commit

---
+++ src/BirthMonth.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class BirthMonth {
+    public static void main(String[] args) {
+
+        int birthMonth = 0;
+        String trash = " ";
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter your birth month (1-12): ");
+        if (in.hasNextInt()) {
+            birthMonth = in.nextInt();
+            in.nextLine();
+
+            if (birthMonth >= 1 && birthMonth <= 12) {
+                System.out.println("Your birth month is " + birthMonth);
+            } else {
+                System.out.println("Invalid month number: " + birthMonth);
+                System.out.println("Exiting... Run the program again");
+                System.exit(0);
+            }
+        } else {
+            trash = in.nextLine();
+            System.out.println("Invalid month input: " + trash);
+            System.out.println("Exiting... Run the program again");
+            System.exit(0);
+
+
+        }
+    }
+}
